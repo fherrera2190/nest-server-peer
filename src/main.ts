1b037c80e3c178942133337f03a8e61abbe28dc3
Fernando Herrera <herrera.fernando1209@gmail.com>
2024-10-30 19:57:27 -0300

    add peer support

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as express from 'express';
+import { ExpressPeerServer } from 'peer';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   const port: number = +process.env.PORT || 3000;
   await app.listen(port);
   console.log(`Application is running on: http://localhost:${port}`);
+
+  // Peer Server
+  const peerApp = express();
+  const peerServer = peerApp.listen(50000);
+  const peerExpressServer = ExpressPeerServer(peerServer);
+  peerApp.use('/peerjs', peerExpressServer);
 }
 bootstrap();
