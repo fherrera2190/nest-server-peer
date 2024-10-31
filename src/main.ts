64ad81d566f9aeaa7914df09d506fec6b2dabc96
Fernando Herrera <herrera.fernando1209@gmail.com>
2024-10-30 23:54:40 -0300

    fix double port on render

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as express from 'express';
 import { ExpressPeerServer } from 'peer';
+
 async function bootstrap() {
-
- // Peer Server
-  const peerApp = express();
-  const portPeer = +process.env.PEER_PORT || 50000;
-  const peerServer = peerApp.listen(portPeer);
-  console.log(`Peer Server running on: http://localhost:${portPeer}`);
-  const peerExpressServer = ExpressPeerServer(peerServer);
-  peerApp.use('/peerjs', peerExpressServer);
-
-
-
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   const port: number = +process.env.PORT || 51000;
+
+  const peerServer = ExpressPeerServer(app.getHttpServer());
+
+  app.use('/peerjs', peerServer);
+
   await app.listen(port);
   console.log(`Application is running on: http://localhost:${port}`);
-
- 
 }
 bootstrap();
